cd2a556f0f48b262f139bbfd3c7b82fe94cd193a
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-08-28 21:35:32 +0300

    Initial commit

---
+++ map1.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
+int clientsCount = 1;
+void clientsThread() {
+	while (clientsCount != 10) {
+		std::this_thread::sleep_for(1000ms);
+		++clientsCount;
+		std::cout << "clientsCount from clientsThread: " << clientsCount << "\n";
+	}
+}
+
+void operatorTread() {
+	while (clientsCount != 0) {
+		std::this_thread::sleep_for(2000ms);
+		--clientsCount;
+		std::cout << "clientsCount from operatorTread: " << clientsCount << "\n";
+	}
+
+}
+int main() {
+	std::thread t1(clientsThread);
+	std::thread t2(operatorTread);
+	t1.join();
+	t2.join();
+	return 0;
+}
